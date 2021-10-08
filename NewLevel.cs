dc5a6056200237c03c01a3adb160247c12b7233e
MarlKarx <wmatychuk@zoho.com>
2021-10-08 09:00:40 -0600

    Add project files.

---
+++ NewLevel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonkeyKong
+{
+    public partial class NewLevel : Form
+    {
+        Canvas canvas;
+        public NewLevel()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            canvas = new Canvas();
+            canvas.Show();
+        }
+
+        private void NewLevel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
